74f8634043f4b1529309913cfe3c256b4b38409e
montgomeri15 <montgomeri15@gmail.com>
2022-11-10 20:11:37 +0200

    First commit

---
+++ src/main/java/org/example/Cat.java
@@ -0,0 +1,7 @@
+package org.example;
+
+import java.util.Objects;
+
+public class Cat extends Predator {
+
+}
---
+++ src/main/java/org/example/Predator.java
@@ -0,0 +1,7 @@
+package org.example;
+
+import java.util.Objects;
+
+public abstract class Predator {
+
+}
